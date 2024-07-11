b4d50026c9cfa63df2c2dc35303a6261cd900afc
RoberioAlves <roberiouol@gmail.com>
2024-07-11 10:39:39 -0300

    api no projeto adc

--- src/containers/Login/index.jsx
+++ src/containers/Login/index.jsx
@@ -1,7 +1,8 @@
-import { useForm } from "react-hook-form"
-import { yupResolver } from "@hookform/resolvers/yup"
-import * as yup from "yup"
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
 import Logo from '../../assets/Logo 1.png';
+import { api } from '../../services/api';
 import {
   Container,
   Form,
@@ -16,22 +17,36 @@ import { Button } from '../../components/Button';
 
 export function Login() {
   const schema = yup
-  .object({
-    email: yup.string().email('Digite um email válido!').required("O email é obrigatório"),
-    password: yup.string().min(6, 'A senha deve ter no minimo 6 caractere').required('Digite a senha correta.'),
-  })
-  .required();
-  
-    const {
-      register,
-      handleSubmit,
-      formState: { errors },
-    } = useForm({
-      resolver: yupResolver(schema),
+    .object({
+      email: yup
+        .string()
+        .email('Digite um email válido!')
+        .required('O email é obrigatório'),
+      password: yup
+        .string()
+        .min(6, 'A senha deve ter no minimo 6 caractere')
+        .required('Digite a senha correta.'),
     })
-    const onSubmit = (data) => console.log(data)
+    .required();
 
-    console.log(errors)
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  console.log(errors);
+
+  const onSubmit = async data => {
+    const response = await api.post('/session', {
+      email: data.email,
+      password: data.password,
+    });
+
+    console.log(response);
+  };
 
   return (
     <Container>
@@ -46,20 +61,20 @@ export function Login() {
         <Form onSubmit={handleSubmit(onSubmit)}>
           <InputContainer>
             <label>Email</label>
-            <input type="email" {...register("email")} />
+            <input type="email" {...register('email')} />
             <p>{errors?.email?.message}</p>
           </InputContainer>
           <InputContainer>
-          <label>Senha</label>
-            <input type="password" {...register("password")} />
+            <label>Senha</label>
+            <input type="password" {...register('password')} />
             <p>{errors?.password?.message}</p>
             <Link>
-            <a href="http://" target="_blank" rel="noopener noreferrer">
-              Esquecir minha senha
-            </a>
-          </Link>
+              <a href="http://" target="_blank" rel="noopener noreferrer">
+                Esquecir minha senha
+              </a>
+            </Link>
           </InputContainer>
-          
+
           <Button type="submit">Entrar</Button>
         </Form>
         <Link>
